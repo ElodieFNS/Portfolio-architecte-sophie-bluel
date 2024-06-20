393cc477eb00a3258ac474eca5d494b6f022a878
Elodie Fernandes <fernandeselodiepro@gmail.com>
2024-06-20 18:27:52 +0200

    modification du js pour le formulaire de connexion

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -118,45 +118,46 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginForm').addEventListener('submit', function(event){
+        event.preventDefault(); // empeche l'envoi par défaut du formulaire
 
-document.getElementById('loginForm').addEventListener('submit', function(event){
-    event.preventDefault(); // empeche l'envoi par défaut du formulaire
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+        const apiUrlLogin = 'http://localhost:5678/api/users/login';
 
-    const apiUrlLogin = 'http://localhost:5678/api/users/login';
+        const data = { //envoies de données à l'api sous forme json
+            email: email,
+            password: password
+        };
 
-    const data = { //envoies de données à l'api sous forme json
-        email: email,
-        password: password
-    };
+        const requestOptions = { //option pour la requete fetch
+            method: 'POST', //méthode post pour envoyer données
+            headers: {
+                'Content-Type': 'application/json' //défini que tu JSON est envoyé
+            },
+            body: JSON.stringify(data) //converti l'objet "data" en JSION pour l'envoie
+        };
 
-    const requestOptions = { //option pour la requete fetch
-        method: 'POST', //méthode post pour envoyer données
-        headers: {
-            'Content-Type': 'application/json' //défini que tu JSON est envoyé
-        },
-        body: JSON.stringify(data) //converti l'objet "data" en JSION pour l'envoie
-    };
+        fetch(apiUrlLogin, requestOptions) //envoie requete POST
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur de connexion');
+            }
+            return response.json(); // Conversion de la réponse en JSON
+        })
+        .then(data => { //traite les données JSON renvoyées par l'API après une connexion réussie
+            localStorage.setItem('token', data.token); //stock token d'authentificztion dans "localStorage"
+            console.log('Connexion réussie');
+            //window.location.href = '/dashboard.html';
+        })
+        .catch(error => {
+            console.error('Erreur:', error);
+            document.getElementById('error-message').textContent = 'Identifiants incorrects. Veuillez réessayer.';
+        });
 
-    fetch(apiUrlLogin, requestOptions) //envoie requete POST
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erreur de connexion');
-        }
-        return response.json(); // Conversion de la réponse en JSON
-    })
-    .then(data => { //traite les données JSON renvoyées par l'API après une connexion réussie
-        localStorage.setItem('token', data.token); //stock token d'authentificztion dans "localStorage"
-        console.log('Connexion réussie');
-        //window.location.href = '/dashboard.html';
-    })
-    .catch(error => {
-        console.error('Erreur:', error);
-        document.getElementById('error-message').textContent = 'Identifiants incorrects. Veuillez réessayer.';
     });
-
 });
 // Appelle la fonction fetchData pour démarrer le processus
 fetchData();
